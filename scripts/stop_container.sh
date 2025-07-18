4aad2eeb0f42e14d1ce4a54ab560fce81aecbedb
SaakshiMehrish1 <saakshiupskill@gmail.com>
2025-07-18 08:35:59 +0000

    small edit

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,5 +3,5 @@ set -e
 
 # Stop the running container (if any) 
 # This will print output of containers running with container id
-conatiner_id=`sudo docker ps | awk -F " " '{print $1}'`
+conatiner_id=`sudo docker ps | awk -F" " '{print $1}'`
 sudo docker rm -f $conatiner_id
