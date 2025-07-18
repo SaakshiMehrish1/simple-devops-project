7772c87763b27594e38ab31762f4168cae33daf7
SaakshiMehrish1 <saakshiupskill@gmail.com>
2025-07-18 08:23:21 +0000

    added stop_container

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Stop the running container (if any) 
+# This will print output of containers running with container id
+conatiner_id=`sudo docker ps | awk -F " " '{print $1}'`
+sudo docker rm -f $conatiner_id
